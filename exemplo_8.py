789b2edc797f20306231f3d9955784ed507ad2e7
Luís Eduardo Guollo Bertan <luisbertan@gmail.com>
2019-10-17 16:59:05 -0300

    novos

--- exemplo_8.py
+++ exemplo_8.py
@@ -1,17 +1,14 @@
-n=int(input("Diga o npumero de Atletas :\n"))
-raking=[]
-higth=[]
-age=[]
-print("Diga a posição de cada candidato: \n")
+n=int(input("Diga o número de Atletas :\n"))
+candidatos=[]
+print("Insira o nome, colocação, altura e idade do competidor, respectivamente.")
 for i in range (0,n):
-    pos=input()
-    ranking.append(pos)
-print("Diga os valores das respectivas alturas:\n")
-for i in range (0,n):
-    alt=input()
-    higth.append(alt)
-print("Diga o valor das respectivas idades dos membros: \n")
-for i in range (0,n):
-    idade=input()
-    age.append(idade)
-x=input("Diga qual o critério que deve ser classificado (Altura=1 Idade=2) \n")
+    cand=[input("Nome: "),input("Colocação: "),input("Altura: "),input("Idade: ")]
+    candidatos.append(cand)
+x=str(input("Como você gostaria de classificar os membros? (altura | idade )\n"))
+ordenar=[]
+if x=="altura":
+    ordenar=sorted(candidatos,key=lambda candidatos: candidatos[2])
+    print(ordenar)
+if x=="idade":
+    ordenar=sorted(candidatos,key=lambda candidatos: candidatos[3])
+    print(" A lista ordenada por ",x, "em ordem crescente é ",ordenar)
